0e455eacdfa4eb25b1944b03d27f4a633838aab6
Austin <akingsley319@yahoo.com>
2022-05-11 17:12:31 -0500

    create game files 2017-2021

---
+++ create_gamefiles.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+""" Get all of the Data """
+
+import get_data as ga
+import pandas as pd
+
+# Grabs all game data from 2017 through 2021 season for all teams
+ga.game_files(seasons=(2017,2021))
+
+df = pd.read_csv('data/game_files.csv', sep=';', encoding="latin-1")
